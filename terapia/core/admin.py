4e98c6f1e682381c74c73bb56cd3e79677ad5f90
lucasgrossosouza <seuemail@exemplo.com>
2024-06-06 20:06:14 -0300

    Teste commit

--- terapia/core/admin.py
+++ terapia/core/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 
-from .models import Terapeuta
-admin.site.register(Terapeuta)
+from .models import Profile, Sessao, Contatos
+
+admin.site.register(Profile)
+admin.site.register(Sessao)
+admin.site.register(Contatos)
--- terapia/urls.py
+++ terapia/urls.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 from .core import views as core_views, urls as core_urls
+
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    path ('', core_views.homepage, name='homepage'),
-    path('login', core_views.login, name='login'),
-
-    # Include core
+    path("accounts/", include("allauth.urls")),
+    path("login/", core_views.Login.as_view(), name="login"),
     path("core/", include(core_urls)),
 
+
+
+
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
