4eb8fd313279b888fa710a34a667dadfe59790cc
Vlozo <alevelozo26@gmail.com>
2024-08-23 19:14:20 -0300

    Update Settings

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
